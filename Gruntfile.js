71413aa18d107b700167ff68bd847ce01816e63b
Gen Liu <gerald_721@hotmail.com>
2014-08-21 10:27:13 +0800

    add usemin livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,52 @@ module.exports = function (grunt) {
   grunt.initConfig({
     config: appConfig,
 
+    wiredep: {
+      dev: {
+        src: '<%= config.app %>/index.html',
+        exclude: ['less'],
+        ignorePath: '..'
+      },
+      dist: {
+        src: '<%= config.app %>/_index.html',
+        exclude: ['less']
+      }
+    },
+
+    useminPrepare: {
+      html: '<%= config.app %>/_index.html',
+      options: {
+        dest: '<%= config.dist %>'
+      }
+    },
+
+    usemin: {
+      html: '<%= config.app %>/_index.html',
+      options: {
+        blockReplacements: {
+          less: function (block) {
+            return '<link rel="stylesheet" href="' + block.dest + '">';
+          }
+        }
+      }
+    },
+
+    watch: {
+      dev: {
+        files: '<%= config.app %>/**/*.*',
+        options: {
+          livereload: true
+        }
+      }
+    },
+
     connect: {
       options: {
         port: 9000
       },
       dev: {
         options: {
-          keepalive: true,
+          livereload: true,
           middleware: function (connect) {
             return [
               connect().use('/bower_components', connect.static('bower_components')),
@@ -37,18 +76,40 @@ module.exports = function (grunt) {
     copy: {
       dev: {
         src: '<%= config.app %>/index.html',
+        dest: '<%= config.app %>/_index.html'
+      },
+      dist: {
+        src: '<%= config.app %>/_index.html',
         dest: '<%= config.dist %>/index.html'
       }
     },
 
     clean: {
+      temp: {
+        src: '<%= config.app %>/_index.html'
+      },
       dist: {
-        src: '<%= config.dist%>'
+        src: '<%= config.dist %>'
       }
     }
 
   });
 
-  grunt.registerTask('build', ['clean', 'less', 'copy']);
-  grunt.registerTask('default', ['connect']);
+  grunt.registerTask('build', [
+    'clean:dist',
+    'copy:dev',
+    'wiredep:dist',
+    'useminPrepare',
+    'concat:generated',
+    'cssmin:generated',
+    'uglify:generated',
+    'usemin',
+    'copy:dist',
+    'clean:temp',
+    'less'
+  ]);
+
+  grunt.registerTask('serve', ['wiredep:dev', 'connect:dev', 'watch:dev']);
+
+  grunt.registerTask('default', ['serve']);
 };
